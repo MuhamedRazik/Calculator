f61b99592316f547ad7b36463ce102131386162f
Muhamed Razik <muhammedrazik987@gmail.com>
2024-11-12 10:18:04 +0530

    basic math operation function add

---
+++ index.js
@@ -0,0 +1,20 @@
+const add = function (value1, value2) {
+    return value1 + value2;
+};
+
+const subtract = function (value1, value2) {
+    return value1 - value2;
+};
+
+const multiply = function (value1, value2) {
+    return value1 * value2;
+};
+
+const divide = function (value1, value2) {
+    return value1 / value2;
+};
+
+console.log(add(1,2));
+console.log(subtract(1,2));
+console.log(multiply(1,2));
+console.log(divide(1,2));
