20bd887f4f32d598b7b3e9fdbd3d683e09b588fd
Muhamed Razik <muhammedrazik987@gmail.com>
2024-11-12 22:11:51 +0530

    DOM section of buttons.
    all button selected through dom manipulation

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 const allBtn = document.querySelectorAll('button');
-// console.log(allBtn);
+
+const allClearBtn = document.getElementById('all-clear');
+const clearBtn = document.getElementById('clear');
+
+const numOneBtn = document.getElementById('num-1');
+const numTwoBtn = document.getElementById('num-2');
+const numThreeBtn = document.getElementById('num-3');
+const numFourBtn = document.getElementById('num-4');
+const numFiveBtn = document.getElementById('num-5');
+const numSixBtn = document.getElementById('num-6');
+const numSevenBtn = document.getElementById('num-7');
+const numEightBtn = document.getElementById('num-8');
+const numNineBtn = document.getElementById('num-9');
+const numZeroBtn = document.getElementById('num-zero');
+const dotBtn = document.getElementById('dot-symbol');
+
+const equalBtn = document.getElementById('equal-btn');
+const addBtn = document.getElementById('add-btn');
+const subtractBtn = document.getElementById('subtract-btn');
+const multiplyBtn = document.getElementById('multiply-btn');
+const divideBtn = document.getElementById('divide-btn');
+
+const display = document.getElementById('cal-display');
+
+// hover effect for all button
 allBtn.forEach(button => {
     button.addEventListener("mouseover", () => {
         button.style.color = "white";
