426a7590c9a4e14dfc0a6f139aa0f629d3470ac6
Muhamed Razik <muhammedrazik987@gmail.com>
2024-11-12 10:26:17 +0530

    operate functon added.
    variable for number, an operator, and another number created

--- index.js
+++ index.js
@@ -13,8 +13,26 @@ const multiply = function (value1, value2) {
 const divide = function (value1, value2) {
     return value1 / value2;
 };
+// console.log(add(1,2));
+// console.log(subtract(1,2));
+// console.log(multiply(1,2));
+// console.log(divide(1,2));
 
-console.log(add(1,2));
-console.log(subtract(1,2));
-console.log(multiply(1,2));
-console.log(divide(1,2));
+let firstValue = '';
+let operator = '';
+let secondValue = '';
+
+const operate = function (firstValue,operator,secondValue) {
+    if (operator == '+') {
+        return add(firstValue, secondValue) ;
+    }
+    else if (operator == '-') {
+        return subtract(firstValue, secondValue) ;
+    }
+    else if (operator == '*') {
+        return multiply(firstValue, secondValue) ;
+    }
+    else if (operator == '/') {
+        return divide(firstValue, secondValue) ;
+    };
+};
